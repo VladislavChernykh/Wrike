d6d37f3ef6a2a18555b34ee30b4dd5d61b38778a
VladislavChernykh <vlad.chernykh@mail.ru>
2019-02-16 17:20:20 +0300

    Short cssSelector; minor changes

--- selenium/src/main/java/com/wrike/selenium/steps/Second.java
+++ selenium/src/main/java/com/wrike/selenium/steps/Second.java
@@ -8,7 +8,7 @@ import com.wrike.selenium.pages.LoginPage;
 public class Second {
 	WebDriver driver;
 	LoginPage loginPage;
-	By loginButtonLocator = By.cssSelector("body > div.wg-layout.wg-layout--outline > header > div.wg-header__desktop > div.wg-header__sticky-menu > div > div > div.wg-header__cell.wg-header__cell--xs-3.wg-header__cell--sm-6.wg-header__cell--md-5.wg-header__cell--xl-5 > div > form > button");
+	By loginButtonLocator = By.cssSelector("div.wg-header__cell.wg-header__cell--xs-3.wg-header__cell--sm-6.wg-header__cell--md-5.wg-header__cell--xl-5 > div > form > button");
 	
 	public Second (WebDriver driver){
         this.driver = driver;
--- selenium/src/main/java/com/wrike/selenium/steps/Third.java
+++ selenium/src/main/java/com/wrike/selenium/steps/Third.java
@@ -11,7 +11,6 @@ import com.wrike.selenium.pages.LoginPage;
 public class Third {
 	WebDriver driver;
 	LoginPage loginPage;
-	By loginButtonLocator = By.cssSelector("body > div.wg-layout.wg-layout--outline > header > div.wg-header__desktop > div.wg-header__sticky-menu > div > div > div.wg-header__cell.wg-header__cell--xs-3.wg-header__cell--sm-6.wg-header__cell--md-5.wg-header__cell--xl-5 > div > form > button");
 	
 	public Third (WebDriver driver){
         this.driver = driver;
@@ -23,9 +22,8 @@ public class Third {
 	
 	private String getEmail() {
 		String template = "+wpt@wriketask.qaa";	
-		String random = generateRandomText();
-		String email = random + template;
-
+		String random 	= generateRandomText();
+		String email 	= random + template;
 		return email;
 	}
 	
