88d939b0a7e8a9d05bb52c6649360a388082dece
VladislavChernykh <vlad.chernykh@mail.ru>
2019-02-16 16:35:57 +0300

    1 and 2 steps are fixed

--- selenium/src/main/java/com/wrike/selenium/test/App.java
+++ selenium/src/main/java/com/wrike/selenium/test/App.java
@@ -20,15 +20,23 @@ public class App {
     }
     
     private static void doSteps(WebDriver driver) {
+    	//1. Open url: wrike.com;
     	First first = new First(driver);
     	first.main();
     	System.out.println("First step done");
     	
+    	//2. Click "Get started for free" button near "Login" button;
     	Second second = new Second(driver);
     	second.main();
     	System.out.println("Second step done");
     	
-        System.out.println("Success");
+    	//3. Fill in the email field with random generated value of email with mask “<random_text>+wpt@wriketask.qaa”​ (e.g. “​abcdef+wpt@wriketask.qaa”​ );
+    	//4. Click on "Create my Wrike account" button + check with assertion that you are moved to the next page;
+    	//5. Fill in the Q&A section at the left part of page (like random generated answers) + check with assertion that your answers are submitted;
+    	//6. Click on "Resend email" + check it with assertion;
+    	//7. Check that section "Follow us" at the site footer contains the "Twitter" button that leads to the correct url and has the correct icon.
+        
+    	System.out.println("Success");
     }
     
     private static void setSystemProperties() {
