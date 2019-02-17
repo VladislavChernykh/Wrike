dcdca4508c3f9b2a199f601e8a15041a386b8f37
VladislavChernykh <vlad.chernykh@mail.ru>
2019-02-17 16:20:03 +0300

    Delete extra variables, minor changes

--- selenium/src/main/java/com/wrike/selenium/steps/Second.java
+++ selenium/src/main/java/com/wrike/selenium/steps/Second.java
@@ -1,6 +1,5 @@
 package com.wrike.selenium.steps;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 import com.wrike.selenium.pages.LoginPage;
@@ -8,7 +7,6 @@ import com.wrike.selenium.pages.LoginPage;
 public class Second {
 	WebDriver driver;
 	LoginPage loginPage;
-	By loginButtonLocator = By.cssSelector("div.wg-header__cell.wg-header__cell--xs-3.wg-header__cell--sm-6.wg-header__cell--md-5.wg-header__cell--xl-5 > div > form > button");
 	
 	public Second (WebDriver driver, LoginPage loginPage){
         this.driver = driver;
